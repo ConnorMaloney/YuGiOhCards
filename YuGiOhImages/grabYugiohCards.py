f9616dd362bb14c690871e7f90dc9f0f6b947ddd
Foopert <foopert@gmail.com>
2019-02-08 01:44:23 -0500

    Cleaned up code and removed redundant condition

--- YuGiOhImages/grabYugiohCards.py
+++ YuGiOhImages/grabYugiohCards.py
@@ -3,10 +3,11 @@ import time
 import threading
 import math
 
-# stopped at 10011000
+# Define boundaries
 minImgUrlIndex = 10000000
 maxImgUrlIndex = 100000000
 
+# Scraping function
 def getCards(start, end):
     print("Scanning cards for ranges: ", start, " to ", end)
     for x in range(start, end):
@@ -14,20 +15,19 @@ def getCards(start, end):
             r = requests.get('https://ygoprodeck.com/pics/' + str(x) + '.jpg')
             if ('Content-length' in r.headers):
                 with open(str(x) + '.jpg', 'wb') as f:
-                    if ('Content-length' not in r.headers):
-                        print('no content')
-                    else:
-                        f.write(r.content)
-                        print(x, ': ' + r.headers['Content-type'], ' size: ', r.headers['Content-length'])
+                    f.write(r.content)
+                    print(x, ': ' + r.headers['Content-type'], ' size: ', r.headers['Content-length'])
             else:
-                print(x, ' ', r.headers['Content-type'], "   ")
+                print(x, ' ', r.headers['Content-type'], " [NO CARD]")
         except:
             print('exception, quitting')
             quit()
 
+# Define threads
 thread_count = 100
 thread_list = []
 
+# Assign thread ranges
 for i in range(thread_count):
     if (i <= 10):
         start = math.floor(i * ((maxImgUrlIndex-minImgUrlIndex)/thread_count)) + 1 + minImgUrlIndex          
@@ -38,7 +38,6 @@ for i in range(thread_count):
         end = math.floor((i + 1) * ((maxImgUrlIndex-minImgUrlIndex)/thread_count)) + 1 + minImgUrlIndex
         thread_list.append(threading.Thread(target=getCards, args=(start, end)))
 
-
 for thread in thread_list:
     thread.start()
 
@@ -46,28 +45,3 @@ for thread in thread_list:
     thread.join()
 
 getCards(minImgUrlIndex, maxImgUrlIndex)
-
-"""
-thread_count = 8
-thread_list = []
-
-for i in range(thread_count):
-    # Conditions so that thread 0 does not start at 0 index
-    if (i == 0):
-        start = minImgUrlIndex          
-        end   = int((maxImgUrlIndex - minImgUrlIndex) / 8)
-        thread_list.append(threading.Thread(target=getCards, args=(start, end)))
-    else:
-        start = math.floor(i * ((maxImgUrlIndex-minImgUrlIndex)/thread_count)) + 1          
-        end = math.floor((i + 1) * ((maxImgUrlIndex-minImgUrlIndex)/thread_count)) + 1 
-        thread_list.append(threading.Thread(target=getCards, args=(start, end)))
-
-
-for thread in thread_list:
-    thread.start()
-
-for thread in thread_list:
-    thread.join()
-
-getCards(10011000, maxImgUrlIndex)
-"""    
